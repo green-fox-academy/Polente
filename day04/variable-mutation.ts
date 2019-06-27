7644aeda0c21928972fbc7488c31454f4781cc32
Ege Durukan <egedurukan@gmail.com>
2019-06-27 16:13:46 +0200

    variable-mutation

---
+++ day04/variable-mutation.ts
@@ -0,0 +1,33 @@
+let a: number= 3
+    console.log(a + 10)
+
+let b: number= 100
+    console.log(b - 7)
+
+let c: number= 44
+    console.log(c * 2)
+
+let d: number= 125
+    console.log(d / 5)
+
+let e: number= 8
+    console.log(Math.pow(e, 3))
+
+let f1: number= 123
+let f2: number= 345
+    console.log(f1 > f2)
+
+let g1: number=350
+let g2: number= 200
+    console.log( (2 * g2) > g1 )
+
+let h: number= 1357988018575474
+    console.log(Boolean(h / 11))
+
+let i1: number = 10;
+let i2: number = 3;
+    console.log( (Math.pow (3, 2) < i1 && (Math.pow(3, 3) > i1)))
+
+let j: number = 1521;
+    console.log( Boolean(j / 3 || j / 5))
+
