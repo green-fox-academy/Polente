bb8efb2e2415eaf63fb0e955acb9480505342c78
Ege Durukan <egedurukan@gmail.com>
2019-06-27 14:44:32 +0200

    basic information

---
+++ day04/basic-info.ts
@@ -0,0 +1,16 @@
+let myName: string= "Ege"
+let myAge: number= 23
+let myHeight: number= 1.75
+let myMarrige: boolean= false
+
+console.log("My name is " + myName)
+console.log("I am " + myAge + " years old")
+console.log("My height is " + myHeight)
+
+if(myMarrige == false){
+
+        console.log("I am not married.")
+}
+else{
+    console.log("I am married.")
+}
