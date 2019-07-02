02da91c8dbe88a8694f1913f2a40ba102611a484
Ege Durukan <egedurukan@gmail.com>
2019-07-02 17:23:06 +0200

    last edit for previous tasks

---
+++ week02/day02/map-into.ts
@@ -0,0 +1,15 @@
+'use strict'
+
+let map = {}
+
+
+map [97] = "a"
+map [98] = "b"
+map [99] = "c"
+map [65] = "A"
+map [66] = "B"
+map [67] = "C"
+map [68] = "D"
+
+
+console.log(map);
--- week02/day02/todo-print.ts
+++ week02/day02/todo-print.ts
@@ -5,8 +5,9 @@ let todoText: string[] = [' - Buy milk'];
 
 todoText[1]= todoText[0]
 todoText[0]="My todo:"
-todoText[2]= ' - Download games'
-todoText[3]= "\t- Diablo"
+todoText.push("- Download games", "\t- Diablo")
+//todoText[2]= ' - Download games'
+//todoText[3]= "\t- Diablo"
 
 
 
@@ -14,3 +15,4 @@ todoText.forEach(function(theList){
 
     console.log(theList)
 });
+
