8d648c90286ae6983c25b7ebd520bd33657f4981
Ege Durukan <egedurukan@gmail.com>
2019-07-09 16:16:15 +0200

    copy-file and destination exercises added

---
+++ week03/day02/copy-file.ts
@@ -0,0 +1,16 @@
+"use strict";
+
+declare function require(path: string): any;
+const fs = require("fs");
+
+function copier(a, b) {
+  try {
+    let content = fs.readFileSync(`${a}`, "utf-8");
+    fs.writeFileSync(`${b}`, `${content}`);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+console.log(copier("apple.txt", "destination.txt"));
