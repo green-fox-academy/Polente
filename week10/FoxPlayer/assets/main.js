a1e7ebf5cffb10f3d47bd56f150d7d95851c2a1f
Ege Durukan <egedurukan@gmail.com>
2019-09-09 15:31:28 +0200

    grid been made. Overall structure of the html file has been made, logo added

--- week10/FoxPlayer/assets/main.js
+++ week10/FoxPlayer/assets/main.js
@@ -3,6 +3,17 @@
 const body = document.querySelector('body');
 const audio = document.querySelector('audio');
 
-audio.addEventListener('onloadstart', event => {
-  alert('onloadstart happened');
+audio.addEventListener('play', event => {
+  console.log(`${event.type} happened`);
 });
+audio.addEventListener('loadstart', event => {
+  console.log(`${event.type} happened`);
+});
+audio.addEventListener('progress', event => {
+  console.log(`${event.type} happened`);
+});
+audio.addEventListener('ended', event => {
+  console.log(`${event.type} happened`);
+});
+
+audio.setAttribute('src', './music/Organoid_-_09_-_Purple_Drift.mp3');
