5d74cb9be48107c8f2e72182eff54f4749c5b556
Ege Durukan <egedurukan@gmail.com>
2019-09-25 13:05:29 +0200

    hero component had been styled and functionaties added. Hero-detail component generated

---
+++ angular-tour-of-heros/src/app/heros/heros.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from '../mock-heros';
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent implements OnInit {
+
+  selectedHero: Hero;
+  heroes = HEROES;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
